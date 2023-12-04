85351b8c20ec1970a711c1e877d8097246681595
Douglas Souza <lyricsan@hotmail.com>
2023-12-04 16:32:50 -0300

    displaying and working on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="h-screen w-full">
+      <Navbar />
+      <main className="container mx-auto w-full px-5">content</main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <header className="w-full h-20  border-b border-indingo-200">
+      <div className="container mx-auto px-5 flex items-center justify-between h-full">
+        <h1>Book storage</h1>
+        <button className="border rounded p-2">theme</button>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
